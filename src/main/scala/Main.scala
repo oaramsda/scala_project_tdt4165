163247b65509b0ef996d8cf75f2a448d18ba2c1e
oaramsda <olea.ramsdal@gmail.com>
2015-10-28 13:59:44 +0100

    changes in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,14 @@ object Main extends App {
 		t
 	}
 
-  // Write a few transaction examples using Threads
+  	// Write a few transaction examples using Threads
+  	val n = 10
+  	val accs = for (i <- 0 to n) yield {new Account(1000.0)}
+  	//accs.foreach{acc => acc.deposit(Math.round(Math.random*1000): Double)}
+	
+	for (i <- 0 to n) {
+		val t = thread(Bank.transaction(acc1, acc2, 500))
+	}
 
+  	
 }
