29bdc9523f9a775aa74f94dc99fa97c26343b63f
Moritz Munch <mnch.moritz6@gmail.com>
2015-10-28 13:38:35 +0100

    account updated

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -1,7 +1,29 @@
+import exceptions._
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
 
-	def withdraw(amount: Double): Unit = ??? //Implement
-	def deposit(amount: Double): Unit = ??? //Implement
-	def getBalanceAmount: Double = ??? //Implement
-
+  def withdraw(amount: Double) {
+    if (amount >= 0) {
+      if ((initialBalance - amount) < 0) {
+        throw new NoSufficientFundsException();
+      } else {
+        initialBalance -= amount;
+      }
+    }
+    else {
+      throw new IllegalAmountException();
+    }
+  }
+  
+  def deposit(amount: Double) {
+    if (amount >= 0) {
+      initialBalance += amount;
+    } else {
+      throw new IllegalAmountException();
+    }
+  }
+  
+  def getBalanceAmount: Unit = {
+    this.initialBalance;
+  }
 }
