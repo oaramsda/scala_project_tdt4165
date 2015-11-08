3804c54c1e739c0186a80230639d9655d5ac00fa
BourgondAries <macocio@gmail.com>
2015-11-08 14:39:26 +0100

    Improve indentation

--- part2-exercise/src/main/scala/Bank.scala
+++ part2-exercise/src/main/scala/Bank.scala
@@ -4,31 +4,28 @@ import java.util.concurrent.atomic.AtomicLong
 
 class Bank(val allowedAttempts: Integer = 3) {
 
-  private val uid: AtomicLong = new AtomicLong
-  private val transactionsQueue: TransactionQueue = new TransactionQueue()
-  private val processedTransactions: TransactionQueue = new TransactionQueue()
-  private val executorContext = Executors.newFixedThreadPool(100)
+	private val uid: AtomicLong = new AtomicLong
+	private val transactionsQueue: TransactionQueue = new TransactionQueue()
+	private val processedTransactions: TransactionQueue = new TransactionQueue()
+	private val executorContext = Executors.newFixedThreadPool(100)
 
-  def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
-    transactionsQueue push new Transaction(
-      transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
-  }
+	def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+		transactionsQueue push new Transaction(
+			transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
+	}
 
-  def generateAccountId: Long = {
-    uid.incrementAndGet
-  }
+	def generateAccountId: Long = {
+		uid.incrementAndGet
+	}
 
-  private def processTransactions: Unit = ???
+	private def processTransactions: Unit = ???
 
-  def addAccount(initialBalance: Double): Account = {
-    new Account(this, initialBalance)
-  }
+	def addAccount(initialBalance: Double): Account = {
+		new Account(this, initialBalance)
+	}
 
-  def getProcessedTransactionsAsList: List[Transaction] = {
-    processedTransactions.iterator.toList
-  }
-
-	override def run: Unit = {
+	def getProcessedTransactionsAsList: List[Transaction] = {
+		processedTransactions.iterator.toList
 	}
 
 }
