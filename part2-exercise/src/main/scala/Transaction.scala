0339e6d54b79ca42cff9444b56a819de3ec80c8a
BourgondAries <macocio@gmail.com>
2015-11-08 14:10:11 +0100

    Implement most transaction methods.

--- part2-exercise/src/main/scala/Transaction.scala
+++ part2-exercise/src/main/scala/Transaction.scala
@@ -1,4 +1,3 @@
-
 import exceptions._
 import scala.collection.mutable
 
@@ -7,21 +6,43 @@ object TransactionStatus extends Enumeration {
 }
 
 class TransactionQueue {
-  
+
+	object Queue locqueue;
+
   // Remove and return the first element from the queue
-  def pop: Transaction = ???
+  def pop: Transaction = {
+		locqueue synchronized {
+			locqueue dequeue
+		}
+	}
 
   // Return whether the queue is empty
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = {
+		locqueue synchronized {
+			locqueue isEmpty
+		}
+	}
 
   // Add new element to the back of the queue
-  def push(t: Transaction): Unit = ???
+  def push(t: Transaction): Unit = {
+		locqueue synchronized {
+			locqueue enqueue t
+		}
+	}
 
   // Return the first element from the queue without removing it
-  def peek: Transaction = ???
+  def peek: Transaction = {
+		locqueue synchronized {
+			locqueue head
+		}
+	}
 
   // Return an iterator to allow you to iterate over the queue
-  def iterator: Iterator[Transaction] = ???
+  def iterator: Iterator[Transaction] = {
+		locqueue synchronized {
+			locqueue iterator
+		}
+	}
 }
 
 class Transaction(val transactionsQueue: TransactionQueue,
@@ -34,12 +55,12 @@ class Transaction(val transactionsQueue: TransactionQueue,
   var status: TransactionStatus.Value = TransactionStatus.PENDING
 
   override def run: Unit = {
-    
+
     def doTransaction() = {
       from withdraw amount
       to deposit amount
     }
-    
+
     if (from.uid < to.uid) from synchronized {
       to synchronized {
         doTransaction
@@ -49,7 +70,7 @@ class Transaction(val transactionsQueue: TransactionQueue,
         doTransaction
       }
     }
-    
+
     //Extend this method to satisfy new requirements.
 
   }
